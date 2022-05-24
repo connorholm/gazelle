e30a4bc1a479c9409bc7c010fbffe68f79100258
Connor Holm <connorjholm@gmail.com>
2022-05-24 18:54:46 -0500

    weather api integrated with a search bar

--- app/controllers/api/weather_api.rb
+++ app/controllers/api/weather_api.rb
@@ -2,20 +2,25 @@ require 'httparty'
 
 class WeatherApi
     
-    def unique_url
+    def unique_url(q)
         key = ENV["OPEN_WEATHER_API_KEY"]
         
         api_url = "https://api.openweathermap.org/data/2.5/weather"
         
-        query_latlog = { 
-            :"lat" => "44.854031",
-            :"lon" => "-93.460167",
+        # query_latlog = { 
+        #     :"lat" => lat,
+        #     :"lon" => long,
+        #     :"appid" => key,
+        #     :"units" => "imperial"
+        # }
+        query_city = { 
+            :"q" => q,
             :"appid" => key,
             :"units" => "imperial"
         }
         response = HTTParty.get(
             api_url, 
-            :query => query_latlog,
+            :query => query_city,
         )
         json = JSON.parse(response.body)
         json
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,15 +1,28 @@
 require 'net/http'
 require 'api/weather_api'
 class HomeController < ApplicationController
+  helper_method :search_weather
+  
   def index
+    puts "Params: #{params}"
+    if !(params[:city] == nil or params[:city] == "")
+      @data = search_weather(params[:city])
+    end
     # key = ENV["OPEN_WEATHER_API_KEY"]
     
     # url = "https://api.openweathermap.org/data/2.5/weather?lat=44.854031&lon=-93.460167&appid=#{key}"
     # uri = URI(url)
     # response = Net::HTTP.get(uri)
     # @data = JSON.parse(response)
+    # api = WeatherApi.new()
+    # url = api.unique_url
+    # @data = url
+  end
+
+  def search_weather(city)
     api = WeatherApi.new()
-    url = api.unique_url
+    url = api.unique_url(city)
     @data = url
+    url
   end
 end
