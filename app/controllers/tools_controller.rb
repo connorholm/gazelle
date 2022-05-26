67f159ffe8edd38822d6321bafdbdafdb2b4e833
Connor Holm <connorjholm@gmail.com>
2022-05-25 22:55:37 -0500

    topic tagging feature backend built

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -1,13 +1,26 @@
+require 'api/topic_tagging_api'
+
 class ToolsController < ApplicationController
   def index
   end
 
   def tagging
+    puts "Params: #{params}"
+    if !(params[:text] == nil or params[:text] == "")
+      response = get_tags(params[:text])
+      @tagging_response = response
+    end
   end
 
   def keywords
   end
 
   def page_inspect
+  end
+
+  def get_tags(text)
+    api = TopicTaggingApi.new()
+    url = api.tagging_request(text)
+    url
   end
 end
