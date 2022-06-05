e70929b158dae5a3ad6f60fa07775b167faa09c7
Connor Holm <connorjholm@gmail.com>
2022-06-05 15:07:04 -0500

    about page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,15 +9,9 @@ class HomeController < ApplicationController
       response = search_weather(params[:city])
       @data = response
     end
-    # key = ENV["OPEN_WEATHER_API_KEY"]
-    
-    # url = "https://api.openweathermap.org/data/2.5/weather?lat=44.854031&lon=-93.460167&appid=#{key}"
-    # uri = URI(url)
-    # response = Net::HTTP.get(uri)
-    # @data = JSON.parse(response)
-    # api = WeatherApi.new()
-    # url = api.unique_url
-    # @data = url
+  end
+
+  def about
   end
 
   def search_weather(city)
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
     confirmations: 'users/confirmations',
   }
   get 'home/index'
+  get 'home/about'
   root 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
