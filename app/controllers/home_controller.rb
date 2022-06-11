3b6db841104f11afe6acd9a590ab8107f6dd42ee
Connor Holm <connorjholm@gmail.com>
2022-06-11 15:17:42 -0500

    updated home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,7 @@
 require 'net/http'
-require 'api/weather_api'
 class HomeController < ApplicationController
-  helper_method :search_weather
 
   def index
-    puts "Params: #{params}"
-    if !(params[:city] == nil or params[:city] == "")
-      response = search_weather(params[:city])
-      @data = response
-    end
   end
 
   def about
@@ -23,9 +16,4 @@ class HomeController < ApplicationController
   def terms_conditions
   end
 
-  def search_weather(city)
-    api = WeatherApi.new()
-    url = api.unique_url(city)
-    url
-  end
 end
