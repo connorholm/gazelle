4277505daaedf870a41ecea4889c0d7295d56b75
Connor Holm <connorjholm@gmail.com>
2022-06-11 15:04:52 -0500

    update loading module

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,8 @@ Bundler.require(*Rails.groups)
 module Seoapp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0
+    # config.load_defaults 7.0
+    config.autoload = :classic
 
     # Configuration for the application, engines, and railties goes here.
     #
